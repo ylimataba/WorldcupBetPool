070a9c2eed5178fda3cda831a37181b060b6baeb
Matti Yli-Ojanperä <matti.yli-ojanpera@aalto.fi>
2018-06-15 10:19:28 +0300

    added custom command to load matches

---
+++ bets/management/loadmatches.py
@@ -0,0 +1,9 @@
+from bets.database import load_matches
+from django.core.management.base import BaseCommand
+
+class Command(BaseCommand):
+    help = 'Loads matches from football-data API'
+
+    def handle(self, *args, **options):
+        load_matches()
+        self.stdout.write(self.style.SUCCESS('OK'))

59ae1ad74817c5a245d0714cdfc73252e97c314a
Matti Yli-Ojanperä <matti.yli-ojanpera@aalto.fi>
2018-06-15 10:00:19 +0300

    added main

--- bets/database.py
+++ bets/database.py
@@ -87,3 +87,7 @@ def fake_data_load_matches():
                 match_query.update(**match_as_dict)
             else:
                 Match.objects.create(**match_as_dict)
+                
+if __name__=='__main__':
+    load_matches()
+    print('ok')

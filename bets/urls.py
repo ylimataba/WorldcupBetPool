62b346b3d462d1575b08c8f6df60a1095e39a5c5
Matti Yli-Ojanperä <matti.yli-ojanpera@aalto.fi>
2018-07-02 22:29:34 +0300

    adsf

--- bets/urls.py
+++ bets/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     #url(r'^veikkaa/1/$', views.lohkovaihe, name='lohkovaihe'),
     #url(r'^veikkaa/2/$', views.maalikuningas, name='maalikuningas'),
     #url(r'^veikkaa/3/$', views.kolmikko, name='kolmikko'),
-    url(r'^veikkaa/4/$', views.pudotuspelit, name='pudotuspelit'),
+    #url(r'^veikkaa/4/$', views.pudotuspelit, name='pudotuspelit'),
     url(r'^tilanne$', views.tilanne, name='tilanne'),
     url(r'^vertaile$', views.vertaile, name='vertaile'),
 ]
